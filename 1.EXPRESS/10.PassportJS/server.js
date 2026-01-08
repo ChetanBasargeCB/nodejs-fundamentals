b95c82f5bfad85200bc5e2911dec58c6c770010b
ChetanBasargeCB <chetancb9191@gmail.com>
2026-01-08 23:42:08 +0530

    Added Authentication with PassportJs,Where Session is false

--- 1.EXPRESS/10.PassportJS/server.js
+++ 1.EXPRESS/10.PassportJS/server.js
@@ -1,20 +1,33 @@
-//! Authentication learning file
+//! Authentication Practice File
+//? Using Express, MongoDB, Passport
+
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import passport from 'passport'
+import localStrategy from 'passport-local'
 dotenv.config();
 
+//! Step -1 Create Instance
+const local =localStrategy.Strategy
+
+//? Passport Local Strategy Setup
 const app = express();
 const PORT = process.env.PORT || 3000;
 const URI = process.env.URI
 
+
+
+//! Step -2 Initiate Passport Middelware  
 app.use(express.json())
-//! Database Connection
+app.use(express.urlencoded({ extended: true }))
+app.use(passport.initialize())
+
+//?  User Schema Definition
 mongoose.connect(URI)
     .then(() => console.log("DB Connected")).catch((err) => console.log("DB Connection Error", err))
 
-//! Schema Declaration
+//? Schema Declaration
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -26,10 +39,37 @@ const userSchema = new mongoose.Schema({
     },
     password: String,
 });
-//! Model Creation (Collection)
+
+//? Model Creation (Collection)
 const User = mongoose.model("User", userSchema)
 
-//! CURD Opreation
+//! Step-3 Passport Local Authentication Logic 
+passport.use(
+    new local(async(username,password,done)=>{
+        try {
+            console.log("Data of User",username,password)
+            //!  Check if user exists
+            const isUser = await User.findOne({username:username})
+            
+            if(!isUser) {
+                console.log("Invalid Name")
+                return done(null,false,{message:"Invalid Username"})}
+                
+            //! Validate password
+            const isPassword =  isUser.password==password ? true:false
+            if(isPassword){
+                done(null,isUser)
+            } else{
+                 console.log("Incorrect password");
+                done(null,false,{message:"Invalid Password"})
+            }
+
+        } catch (error) {
+            done(error)
+        } 
+})) 
+
+//? CURD Opreation
 const handelAddData = async (name, username, password) => {
     try {
         const data = await User.create({ name: name, username: username, password: password })
@@ -40,17 +80,26 @@ const handelAddData = async (name, username, password) => {
     }
 }
 
+//! Step-4  Apply On Routes
+const authenticate = passport.authenticate("local",{session:false})
 
-app.get("/", (req, res) => {
-    res.send("Welcome to Authentication Basic Concepts");
+//! Protected Route (Requires Login)
+app.get("/", authenticate,(req, res) => {
+    res.send("Welcome to Authentication with PassportJS");
 });
-
+ 
+//?User Login Route
 app.post("/", async (req, res) => {
     const { name, username, password } = req.body
-    const data = await handelAddData(name, username, password)
+    const data = await handelAddData(name, username, password) 
     res.send("Data Added")
     // console.log(name,username,password)
 })
+app.post( "/login", authenticate, (req, res) => {
+    res.send("Login Successful");
+  }
+);
+
 
 app.listen(PORT, () => {
     console.log(`Server Started on port ${PORT}`);
