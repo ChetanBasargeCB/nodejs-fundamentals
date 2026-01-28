ad6ede4b1882aa27a487450cbc3343932e4c90f6
ChetanBasargeCB <chetancb9191@gmail.com>
2026-01-28 17:41:09 +0530

    Added code of for comparison of user ans and correct ans

--- frontedn-AI/Gen-AI/src/Components/Home.jsx
+++ frontedn-AI/Gen-AI/src/Components/Home.jsx
@@ -1,64 +1,91 @@
-import React from 'react'
-import { useState } from 'react'
-import axios from 'axios'
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 export default function Home() {
-    const [data,setData]=useState({question:""})
-    const[output,setOutput]=useState([])
-    const[lodaing,setLoading]=useState(false)
-    
-     const handelInput = (e)=>{
-        setData({question:e.target.value})
+  const [data, setData] = useState({ question: "" });
+  const [output, setOutput] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedAnswers, setSelectedAnswers] = useState({});
 
+
+  const handelInput = (e) => {
+    setData({ question: e.target.value });
+  };
+  const handelsubmit = async (e) => {
+    e.preventDefault();
+    if (!data.question) return;
+    setLoading(true);
+    try {
+      const resp = await axios.post("http://localhost:4000/AI", { data });
+      console.log(resp, "server response");
+    setLoading(false)
+      return setOutput(resp.data);
+    } catch (error) {
+      console.log("FE error", error);
+      setLoading(false);
     }
-    const handelsubmit = async(e)=>{
-         e.preventDefault()
-        if(!data.question) return
-        setLoading(true)
-       try {
-        const resp = await axios.post("http://localhost:4000/AI",{data}) 
-        console.log( resp ,"server response")   
-        const quiz = resp.data
-        return setOutput(resp.data)
+    setLoading(false)
+  };
 
-       } catch (error) {
-        console.log("FE error",error)
-       }
-       setLoading(false)
-    
-    }
-   console.log(output,"ok")
-
+  const handelSelect = (questionIndex, optionIndex)=>{
+    setSelectedAnswers(prev=>({
+      [questionIndex]:optionIndex
+    }))
+  }
+ 
   return (
-    <div className='bg-gray-300 min-h-screen flex flex-col items-center'>
-        <div className='mt-10'>
-            <form onSubmit={handelsubmit} className='flex shadow-2xl '> 
-                <input type="text" className='bg-blue-100 outline-none p-2 rounded-xs' 
-                 value={data.question} onChange={handelInput} placeholder='enter the topic name'  />
-                <button type='submit' className='text-white bg-blue-600 p-2 rounded-xs px-4'>{lodaing?"..Sending":"Send"}</button>
-            </form>
-               {
-                lodaing?<div className='flex justify-center items-center '> 
-                    <p className=' rounded-full w-fit p-2 mt-10  text-blue-500 border-4 border-t-transparent animate-spin '></p>
-                </div>:<div className='hidden'></div>
-            }
-            <div className='mt-10'>
-              {
-                output.map((item,i)=>(
-                      <div key={i} className=' bg-white flex flex-col justify-center p-2  py-4 shadow-2xl rounded-xl '>
-                    <h1>{item.question}</h1>
-                    <ol className='flex flex-col items-center gap-2  '>
-                        <li className='border-gray-300 border w-full text-center p-1 rounded-xs shadow-2xl hover:scale-105'>{item.options[0]}</li>
-                        <li className='border-gray-300 border w-full text-center p-1 rounded-xs shadow-2xl hover:scale-105'>{item.options[1]}</li>
-                        <li className='border-gray-300 border w-full text-center p-1 rounded-xs shadow-2xl hover:scale-105'>{item.options[2]}</li>
-                        <li className='border-gray-300 border w-full text-center p-1 rounded-xs shadow-2xl hover:scale-105'>{item.options[3]}</li>
-                        
-                    </ol>
-                </div>
-                ))
-              }
-            </div>
+    <div className="bg-gray-300 min-h-screen flex flex-col items-center">
+      <div className="mt-10">
+        <form onSubmit={handelsubmit} className="flex shadow-2xl ">
+          <input
+            type="text"
+            className="bg-blue-100 outline-none p-2 rounded-xs"
+            value={data.question}
+            onChange={handelInput}
+            placeholder="enter the topic name"
+          />
+          <button
+            type="submit"
+            className="text-white bg-blue-600 p-2 rounded-xs px-4"
+          >
+            {loading ? "..Sending" : "Send"}
+          </button>
+        </form>
+        {loading ? (
+          <div className="flex justify-center items-center ">
+            <p className=" rounded-full w-fit p-2 mt-10  text-blue-500 border-4 border-t-transparent animate-spin "></p>
+          </div>
+        ) : (
+          <div className="hidden"></div>
+        )}
+        <div className="mt-10">
+          {output &&
+            output.map((item, i) => (
+              <div
+                key={i}
+                className=" bg-white flex flex-col justify-center p-2  py-4 shadow-2xl rounded-xl "
+              >
+                <h1>{item.question}</h1>
+                <ol className="flex flex-col items-center gap-2  ">
+                 {
+                  item.options.map((option,optionIndex)=>(
+                     <li key={optionIndex} className={` ${selectedAnswers[i]!== undefined
+                      ? optionIndex===item.correctAnswer
+                      ? 'bg-green-300':"":"hover:scale-105"
+                     }
+                   border-gray-300 border w-full text-center 
+                   p-1 rounded-xs shadow-2xl hover:scale-105 ` } onClick={()=>handelSelect(i,optionIndex)} >
+                    {option}
+                  </li>
+                  
+                  ))
+                 }
+                </ol>
+              </div>
+            ))}
         </div>
+      </div>
     </div>
-  )
+  );
 }
